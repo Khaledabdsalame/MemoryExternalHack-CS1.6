f8aaf5a1016802f25b696c6d3d6335dc990cd0c4
Khaled Abdsalame <154564727+Khaledabdsalame@users.noreply.github.com>
2024-01-03 14:33:27 +0100

    Update ExtrenalMemoryHack(SourceCode).cpp

--- ExtrenalMemoryHack(SourceCode).cpp
+++ ExtrenalMemoryHack(SourceCode).cpp
@@ -1,5 +1,8 @@
 #include<Windows.h>
 #include<tlhelp32.h>
+#include<stdio.h>
+#include <iostream>
+using namespace std;
 bool GetPid(const wchar_t* targetProcess, DWORD* procID)
 {
     HANDLE snap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
@@ -22,7 +25,7 @@ bool GetPid(const wchar_t* targetProcess, DWORD* procID)
     }
     return false;
 }
-char* GetModuleBase(const wchar_t* ModuleName, DWORD procID){
+char* GetModuleBase(const wchar_t* ModuleName, DWORD procID) {
     MODULEENTRY32 ModuleEntry = { 0 };
     HANDLE SnapShot = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE | TH32CS_SNAPMODULE32, procID);
 
@@ -45,8 +48,21 @@ char* GetModuleBase(const wchar_t* ModuleName, DWORD procID){
     return NULL;
 }
 int main() {
-
-        int  NEW_MONEY_VALUE=9999;
+    int  NEW_MONEY_VALUE = 9999;
+    int  NEW_MONEY_VALUE_1 = 1000;
+    int  NEW_MONEY_VALUE_2 = 16000;
+    int  NEW_MONEY_VALUE_3 = 3000;
+    int  NEW_MONEY_VALUE_4 = 5000;
+    int defaultMONEY = 800;
+    printf("SELECT YOU MONEY BY ROUNDS\n");
+    cout << "PRESS 1 FOR 9999" << "\n";
+    cout << "PRESS 2 FOR 1000" << "\n";
+    cout << "PRESS 3 FOR 16000" << "\n";
+    cout << "PRESS 4 FOR 3000" << "\n";
+    cout << "PRESS 5 FOR 5000" << "\n";
+    cout << "PRESS 0 FOR 800" << "\n";
+    while (true)
+    {
         DWORD ProcId;
         GetPid(L"hl.exe", &ProcId);
         char* DLLBaseAddress = GetModuleBase(L"client.dll", ProcId);
@@ -54,8 +70,44 @@ int main() {
         HANDLE PRocesscs = OpenProcess(PROCESS_ALL_ACCESS, 0, ProcId);
         ReadProcessMemory(PRocesscs, (PBYTE*)BaseAdress, NULL, 4, 0);
         ReadProcessMemory(PRocesscs, (PBYTE*)(BaseAdress + 0x1254D4), NULL, 4, 0);
-        while (true) {
-            WriteProcessMemory(PRocesscs, (PBYTE*)(BaseAdress + 0x1254D4), &NEW_MONEY_VALUE, 4, 0);
+        if (GetAsyncKeyState('1')){
+
+            WriteProcessMemory(PRocesscs, (PBYTE*)(BaseAdress + 0x1254D4), &NEW_MONEY_VALUE , 4, 0);
+           Sleep(2);
+           continue;
         }
         
+        if (GetAsyncKeyState('2')) {
+
+            WriteProcessMemory(PRocesscs, (PBYTE*)(BaseAdress + 0x1254D4), &NEW_MONEY_VALUE_1, 4, 0);
+            Sleep(2);
+            continue;
+        }
+        if (GetAsyncKeyState('3')) {
+
+            WriteProcessMemory(PRocesscs, (PBYTE*)(BaseAdress + 0x1254D4), &NEW_MONEY_VALUE_2, 4, 0);
+            Sleep(2);
+            continue;
+        }
+        if (GetAsyncKeyState('4')) {
+
+            WriteProcessMemory(PRocesscs, (PBYTE*)(BaseAdress + 0x1254D4), &NEW_MONEY_VALUE_3, 4, 0);
+            Sleep(2);
+            continue;
+        }
+        if (GetAsyncKeyState('5')) {
+
+            WriteProcessMemory(PRocesscs, (PBYTE*)(BaseAdress + 0x1254D4), &NEW_MONEY_VALUE_4, 4, 0);
+            Sleep(2);
+            continue;
+        }
+        if (GetAsyncKeyState('0')) {
+
+            WriteProcessMemory(PRocesscs, (PBYTE*)(BaseAdress + 0x1254D4), &defaultMONEY, 4, 0);
+            Sleep(2);
+            continue;
+        }
+
+    }
+
 }
